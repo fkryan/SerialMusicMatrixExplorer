521eabacc7f76b46818336c39d2bf48ca7a43d44
fkryan <fkryan@iu.edu>
2018-05-15 22:45:48 -0500

    visual updates

--- src/GUI.java
+++ src/GUI.java
@@ -4,21 +4,26 @@ import java.awt.*;
 public class GUI extends JFrame {
     public GUI(Matrix matrix) {
         super("Matrix Generator");
-        setSize(500, 500);
+        setSize(1000, 500);
         setLookAndFeel();
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel directions = new JPanel();
+        JLabel greeting = new JLabel("Welcome! Press a row form name to hear it played.");
+        directions.add(greeting);
+        add(directions);
         JPanel mainGrid = new JPanel();
         int gridSize = matrix.size + 2;
         mainGrid.setLayout(new GridLayout(gridSize, gridSize));
         JComponent grid[][] = new JComponent[gridSize][gridSize];
-        grid[0][0] = new JLabel("MATRIX");
-        grid[0][gridSize - 1] = new JLabel("none");
-        grid[gridSize - 1][gridSize - 1] = new JLabel("none");
-        grid[gridSize - 1][0] = new JLabel("none");
+        grid[0][0] = new JLabel("");
+        grid[0][gridSize - 1] = new JLabel("");
+        grid[gridSize - 1][gridSize - 1] = new JLabel("");
+        grid[gridSize - 1][0] = new JLabel("");
         for (int i = 1; i < gridSize - 1; i++) {
             for (int j = 1; j < gridSize - 1; j++) {
-                grid[i][j] = new JLabel(matrix.board[i-1][j-1].toString());
+                grid[i][j] = new JLabel(matrix.board[i-1][j-1].toString(), SwingConstants.CENTER);
+                grid[i][j].setBorder(BorderFactory.createLineBorder(Color.gray));
             }
         }
         for (int j = 1; j < gridSize - 1; j++) {
@@ -36,6 +41,17 @@ public class GUI extends JFrame {
             }
         }
         add(mainGrid);
+        JPanel optionPanel = new JPanel();
+        JComboBox viewOptions = new JComboBox();
+        viewOptions.addItem("Scale Degrees");
+        viewOptions.addItem("Note Names");
+        viewOptions.setEditable(true);
+        JButton regenerate = new JButton("New Random Matrix");
+        JButton restart = new JButton("Enter New Pitch Set");
+        optionPanel.add(viewOptions);
+        optionPanel.add(regenerate);
+        optionPanel.add(restart);
+        add(optionPanel);
         setVisible(true);
     }
 
